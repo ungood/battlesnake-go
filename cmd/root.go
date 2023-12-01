86438b2ba32b5b765dd9c99e2ba0654d66334ddb
Jason Walker <jason@onetrue.name>
2023-12-01 12:13:51 -0800

    Adding structured logging for GCP version of bot

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,17 @@
 package cmd
 
 import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
 	"github.com/spf13/cobra"
+)
+
+var (
+	debug bool
+	gcp   bool
 )
 
 var rootCmd = &cobra.Command{
@@ -10,7 +20,27 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
+	cobra.OnInitialize(initLogging)
 	rootCmd.AddCommand(serveCmd)
+
+	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "Emit debug messages.")
+	rootCmd.PersistentFlags().BoolVar(&gcp, "gcp", false, "Format log messages for Google Cloud Platform.")
+}
+
+func initLogging() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	if gcp {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+		zerolog.LevelFieldName = "severity"
+	} else {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
 }
 
 // Execute executes the root command.
--- server/server.go
+++ server/server.go
@@ -3,65 +3,77 @@ package server
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 
 	"github.com/BattlesnakeOfficial/rules/client"
 	"github.com/ungood/battlesnake-go/actor"
 )
 
-var snake = actor.SimpleSnake{}
+var snake = &actor.SimpleSnake{}
 
-func HandleIndex(w http.ResponseWriter, r *http.Request) {
-	response := snake.Info()
+func decode(r *http.Request, event *zerolog.Event, request interface{}) {
+	err := json.NewDecoder(r.Body).Decode(request)
+	if err != nil {
+		log.Err(err).Msg("Failed to decode request")
+	}
 
+	event.Any("request", request)
+}
+
+func encode(w http.ResponseWriter, event *zerolog.Event, response interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(response)
 	if err != nil {
-		log.Printf("ERROR: Failed to encode info response, %s", err)
+		log.Err(err).Msg("Failed to encode response")
 	}
+
+	event.Any("response", response)
+}
+
+func HandleIndex(w http.ResponseWriter, r *http.Request) {
+	event := log.Info()
+
+	response := snake.Info()
+	encode(w, event, response)
+
+	event.Msg("Handled Info request")
 }
 
 func HandleStart(w http.ResponseWriter, r *http.Request) {
+	event := log.Info()
+
 	request := client.SnakeRequest{}
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		log.Printf("ERROR: Failed to decode start json, %s", err)
-		return
-	}
+	decode(r, event, &request)
 
 	snake.Start(request)
 
-	// Nothing to respond with here
+	event.Msg("Handled Start request")
 }
 
 func HandleMove(w http.ResponseWriter, r *http.Request) {
+	event := log.Info()
+
 	request := client.SnakeRequest{}
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		log.Printf("ERROR: Failed to decode move json, %s", err)
-		return
-	}
+	decode(r, event, &request)
 
 	response := snake.Move(request)
+	encode(w, event, response)
 
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		log.Printf("ERROR: Failed to encode move response, %s", err)
-		return
-	}
+	event.Msg("Handled Move request")
 }
 
 func HandleEnd(w http.ResponseWriter, r *http.Request) {
+	event := log.Info()
+
 	request := client.SnakeRequest{}
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		log.Printf("ERROR: Failed to decode end json, %s", err)
-		return
-	}
+	decode(r, event, &request)
 
 	snake.End(request)
+
+	event.Msg("Handled End request")
 }
 
 const ServerID = "ungood/battlesnake-go"
@@ -83,7 +95,13 @@ func Run(hostname string, port int) {
 	http.HandleFunc("/end", withServerID(HandleEnd))
 
 	addr := fmt.Sprintf("%s:%d", hostname, port)
+	url := fmt.Sprintf("http://%s", addr)
 
-	log.Printf("Running Battlesnake at http://%s\n", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Info().
+		Str("addr", addr).
+		Str("url", url).
+		Msg("Starting server")
+
+	err := http.ListenAndServe(addr, nil)
+	log.Fatal().Err(err).Msg("Failed to start server")
 }
